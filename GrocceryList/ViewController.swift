1bff2e2d13da4c2acf9c47175cdaa5d461624ee5
Logan Peterson <loganp419@yahoo.com>
2023-07-02 22:55:56 -0500

    can now add items to the list

--- GrocceryList/ViewController.swift
+++ GrocceryList/ViewController.swift
@@ -8,27 +8,45 @@
 import UIKit
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-   
+    
     @IBOutlet weak var tableView: UITableView!
     
-    var items = ["A","B","C"]
+    var itemsDestination = ["A","B","C"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
     }
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return items.count
+    @IBAction func AddItem(_ sender: Any) {
+    let alert = UIAlertController(title: "Add an Item", message: nil , preferredStyle: .alert)
+        
+        alert.addTextField { field in
+            field.placeholder = "type here"
+        }
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        alert.addAction(UIAlertAction(title: "Accept", style: .default, handler: { [self] _ in
+            let fields = alert.textFields
+            let itemTextField = fields![0]
+            let items = itemTextField.text!
+           print(items)
+            itemsDestination.append(items)
+       print(itemsDestination)
+            tableView.reloadData()
+        }))
+        self.present(alert, animated: true, completion: nil)
+   
     }
     
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemsDestination.count
+    }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-     
-        cell.textLabel?.text = items[indexPath.row
+        cell.textLabel?.text = itemsDestination[indexPath.row
         ]
-        
         return cell
     }
-
 }
-
