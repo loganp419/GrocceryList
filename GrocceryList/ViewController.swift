e4103ff4def7bafc17eea8aad13b6ec54ff6953f
Logan Peterson <loganp419@yahoo.com>
2023-07-02 22:21:49 -0500

    base list design is done

--- GrocceryList/ViewController.swift
+++ GrocceryList/ViewController.swift
@@ -6,17 +6,29 @@
 //
 
 import UIKit
-//testing Git
-class ViewController: UIViewController {
-    
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+   
     @IBOutlet weak var tableView: UITableView!
     
-   
+    var items = ["A","B","C"]
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
     }
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+     
+        cell.textLabel?.text = items[indexPath.row
+        ]
+        
+        return cell
+    }
 
 }
 
